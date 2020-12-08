1bfecf51a54437f99819b9d4591e3a4936f204f8
alex <crank@gmail.com>
2020-12-07 23:08:44 -0500

    Added selection menu and logout functionality

--- app/src/main/java/com/example/yelpapp/SearchActivity.java
+++ app/src/main/java/com/example/yelpapp/SearchActivity.java
@@ -1,14 +1,68 @@
 package com.example.yelpapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class SearchActivity extends AppCompatActivity {
 
+    private static final String TAG = "SearchActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =getMenuInflater();
+        inflater.inflate(R.menu.activity_menu, menu);
+        return true;
+    }
+
+    //the main activity serves as the login Activity
+    //for the application
+    private void openMainActivity(){
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    //handles when itmes are selected from the menu
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        switch(id){
+            case R.id.action_logout:
+                Toast.makeText(this, "You Have Logged Out", Toast.LENGTH_SHORT).show();
+                AuthUI.getInstance().signOut(this)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful())
+                                {
+                                    openMainActivity();
+                                }
+                                else{
+                                    Log.e(TAG, "onComplete", task.getException());
+                                }
+                            }
+                        });
+
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
